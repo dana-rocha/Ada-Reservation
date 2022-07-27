2018afbe71cd1c0a16d29035ccfaace389caa9cc
Elaine Smith <oneile1219@gmail.com>
2022-07-27 12:20:39 -0700

    <ES&DR> Connecting to Firebase/Firestore; still need to add TS type rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useSyncExternalStore } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
+import {db} from "./firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 
 function App() {
+  const [rooms, setRooms] = useState([]);
+  const roomCollectionsRef = collection(db, "rooms")
+
+  useEffect (() => {
+    const getRooms = async () => {
+    const data = await getDocs(roomCollectionsRef);
+    console.log(data.docs);
+    setRooms(data.docs.map((doc) => (
+      { ...doc.data(), id: doc.id }
+    )));
+  };
+
+  getRooms();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello World</p>
-      </header>
+      {rooms.map((room) => {
+        return (
+          <div>
+            {""}
+            <h1>name: {room.name}</h1>
+          </div>
+        );
+      })}
     </div>
   );
 }
---
+++ src/firebase-config.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    // databaseURL: process.env.REACT_APP_DB,
+    projectId: process.env.REACT_APP_PID,
+    storageBucket: process.env.REACT_APP_SB,
+    messagingSenderId: process.env.REACT_APP_SID,
+    appId: process.env.REACT_APP_APPID
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
