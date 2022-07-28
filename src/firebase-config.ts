e1bb2c1f9dec797cf9353806c13f43e73c5d2590
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-07-28 09:44:15 -0700

    Update meta tag

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -7,7 +7,6 @@ import { User } from "./types/User";
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_APIKEY,
     authDomain: process.env.REACT_APP_AUTHDOMAIN,
-    // databaseURL: process.env.REACT_APP_DB,
     projectId: process.env.REACT_APP_PID,
     storageBucket: process.env.REACT_APP_SB,
     messagingSenderId: process.env.REACT_APP_SID,
