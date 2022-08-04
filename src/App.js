2196fad18a29d2c44f837783808e930f8c084a1f
Elaine Smith <oneile1219@gmail.com>
2022-08-04 13:50:02 -0700

    <ES&DR> React Router routes & navbar complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import adaLogo from "./images/logo.png";
 import NewReservation from "./components/ReservationForm";
 import { addDoc } from "@firebase/firestore";
@@ -6,23 +7,60 @@ import { reservationsCollection } from "./firebase-config";
 import "./App.css";
 
 function App() {
+  return (
+    <div className="App">
+      <header>
+        <img src={adaLogo} alt="Ada Developers Academy logo" id="adaLogo" />
+        <nav id="navbar">
+          <Link to="/" className="links">
+            Login
+          </Link>
+          <Link to="/home" className="links">
+            Home
+          </Link>
+          <Link to="/user" className="links">
+            User
+          </Link>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/user" element={<User />} />
+        </Routes>
+      </main>
+      <footer>© 2022 Ada C17 alum grads - Dana Rocha & Elaine Smith</footer>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <>
+      <p>LOGIN PAGE</p>
+    </>
+  );
+}
+
+function Home() {
   const makeReservation = async (NewSubmission) => {
     // Add a new document in collection "reservations"
     const docRef = await addDoc(reservationsCollection, NewSubmission);
     console.log("New Reservation", docRef.id, NewSubmission);
   };
   return (
-    <div className="App">
-      <header>
-        <img src={adaLogo} alt="Ada Developers Academy logo" id="adaLogo" />
-      </header>
-      <main>
-        <div className="reservation-form">
-          <NewReservation handleSubmission={makeReservation} />
-        </div>
-      </main>
-      <footer>© 2022 Ada C17 alum grads - Dana Rocha & Elaine Smith</footer>
+    <div className="reservation-form">
+      <NewReservation handleSubmission={makeReservation} />
     </div>
+  );
+}
+
+function User() {
+  return (
+    <>
+      <p>USER INFO PAGE</p>
+    </>
   );
 }
 
