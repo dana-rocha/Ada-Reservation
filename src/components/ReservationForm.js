84326bbd09ed9ec46f3c132a921f3d6118a60442
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-08-08 21:45:59 -0700

    OUR BLOCKER IS TIME

--- src/components/ReservationForm.js
+++ src/components/ReservationForm.js
@@ -88,9 +88,12 @@ const NewReservation = (props) => {
 
   // Reservation data
   const reservationComponents = reservations.map((res) => {
-    let datee = new Date(res.date);
-    let newDatee = datee.getDate();
-    return console.log(res.id, res.room, res.timeslot, newDatee);
+    // let datee = new Date(res.date);
+    // let newDatee = datee.getDate();
+    // return console.log(res.id, res.room, res.timeslot, newDatee);
+    // return console.log(res.id, res.date);
+    let myDate = new Date(Number(res.date) * 1000);
+    return console.log(myDate.toLocaleString());
   });
 
   // Submit form & reset back to default
